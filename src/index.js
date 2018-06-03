804416b206e4c2a4b096b4b08b168709ea8d2482
paulopieczarka <paulopieczarka@gmail.com>
2018-06-03 17:13:08 -0300

    Antes das correções.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ const Simplex = require('./simplex/simplex')
 const main = () => 
 {
     console.clear()
-    //test1()
-    //test2()
-    //test3()
-    //test4()
-    test5()
+    // test1()
+    // test2()
+    // test3()
+    // test4()
+    // test5()
+    // test6()
+
+    // let simplex = new Simplex()
+    // simplex.setFn(['max', 1, 1])
+    // simplex.setRestrictions([
+    //     [-1, 1, '>=', 3],
+    //     [1, 1, '<=', 27],
+    //     [-2, 1, '>=', 3]
+    // ])
+    // simplex.solve()
+
+    // let simplex = new Simplex()
+    // simplex.setFn(['min', 1, -1, 2, 0])
+    // simplex.setRestrictions([
+    //     [1, 1, 1, 0, '=', 3],
+    //     [2, -1, 3, 1, '=', 4]
+    // ])
+    // simplex.solve()
+
+    let simplex = new Simplex()
+    simplex.setFn(['min', -1, -1])
+    simplex.setRestrictions([
+        [1, -1, '<=', 4],
+        [-1, 1, '<=', 4]
+    ])
+    simplex.solve()
 }
 
 function test1 () {
@@ -35,7 +61,7 @@ function test3 () {
     let simplex = new Simplex()
     simplex.setFn(['max', 3, 2])
     simplex.setRestrictions([
-        [ 2,  1, '<=', 18],
+        [2,  1, '<=', 18],
         [2,  3, '<=', 42],
         [3, 1, '<=', 24]
     ])
@@ -58,6 +84,16 @@ function test5 () {
     simplex.setRestrictions([
         [-2, 1,  1, '>=', 3],
         [3,  4, 0, '<=', 5]
+    ])
+    simplex.solve()
+}
+
+function test6 () {
+    let simplex = new Simplex()
+    simplex.setFn(['max', 3, 1])
+    simplex.setRestrictions([
+        [2,  1, '<=', 30],
+        [1,  4, '<=', 40]
     ])
     simplex.solve()
 }
